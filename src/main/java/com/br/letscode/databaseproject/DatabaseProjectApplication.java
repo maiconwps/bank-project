ffe027d06175cd5ae2fcb55a8fab81e4d269f9b6
Maicon Pantoja <maiconobmepufpa@gmail.com>
2022-04-10 20:03:00 -0400

    fix: enable JPA auditing.

--- src/main/java/com/br/letscode/databaseproject/DatabaseProjectApplication.java
+++ src/main/java/com/br/letscode/databaseproject/DatabaseProjectApplication.java
@@ -2,8 +2,11 @@ package com.br.letscode.databaseproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DatabaseProjectApplication {
 
 	public static void main(String[] args) {
