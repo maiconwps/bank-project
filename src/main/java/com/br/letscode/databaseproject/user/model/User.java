703dc29d3572d89ca415e9fec5c335369b40ac0e
Maicon Pantoja <maiconobmepufpa@gmail.com>
2022-04-10 20:01:24 -0400

    feat(User): add password and email fields.

--- src/main/java/com/br/letscode/databaseproject/user/model/User.java
+++ src/main/java/com/br/letscode/databaseproject/user/model/User.java
@@ -1,15 +1,21 @@
 package com.br.letscode.databaseproject.user.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Table(name = "usuario")
 @Entity
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,6 +26,12 @@ public class User {
 
     @Column(name = "nome")
     private String name;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "senha")
+    private String password;
 
     @Column(name = "data_criacao")
     @CreatedDate
